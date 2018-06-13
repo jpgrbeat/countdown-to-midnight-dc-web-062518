88a1e58a4326c3ddbdfc1c8a98c6df691264f538
Joshua R Miller <joshua.ross.miller@gmail.com>
2018-06-13 22:23:44 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,11 @@
 #write your code here
 
-def countdown
+def countdown(num)
+  while num > 0 do
+    puts "#{num} SECOND(S)!"
+    num -= 1
+  end
+  puts "HAPPY NEW YEAR!"
 end
+def countdown_with_sleep(num)
+  
